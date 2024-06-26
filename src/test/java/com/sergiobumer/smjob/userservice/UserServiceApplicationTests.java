8260fb5e27f6f459f43b9caa5bea9aed401d71d7
SergioBumer <sibm.95@outlook.com>
2024-06-25 19:41:02 -0500

    Initial commit

---
+++ src/test/java/com/sergiobumer/smjob/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiobumer.smjob.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
